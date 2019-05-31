6745aa1b8c4f2f2a8ba6964b881760445e556cd8
Khoren Markosyan <khoren.markosyan@gmail.com>
2019-05-31 13:56:01 +0400

    Prepare to use animatable model type

--- SwiftHub/Modules/Settings/SettingsSection.swift
+++ SwiftHub/Modules/Settings/SettingsSection.swift
@@ -34,8 +34,36 @@ enum SettingsSectionItem {
     case whatsNewItem(viewModel: SettingCellViewModel)
 }
 
-extension SettingsSection: SectionModelType {
+extension SettingsSectionItem: IdentifiableType {
+    typealias Identity = String
+    var identity: Identity {
+        switch self {
+        case .profileItem(let viewModel): return viewModel.user.login ?? ""
+        case .repositoryItem(let viewModel): return viewModel.repository.fullname ?? ""
+        case .bannerItem(let viewModel),
+             .nightModeItem(let viewModel): return viewModel.model.title ?? ""
+        case .logoutItem(let viewModel),
+             .themeItem(let viewModel),
+             .languageItem(let viewModel),
+             .contactsItem(let viewModel),
+             .removeCacheItem(let viewModel),
+             .acknowledgementsItem(let viewModel),
+             .whatsNewItem(let viewModel): return viewModel.model.title ?? ""
+        }
+    }
+}
+
+extension SettingsSectionItem: Equatable {
+    static func == (lhs: SettingsSectionItem, rhs: SettingsSectionItem) -> Bool {
+        return lhs.identity == rhs.identity
+    }
+}
+
+extension SettingsSection: AnimatableSectionModelType, IdentifiableType {
     typealias Item = SettingsSectionItem
+
+    typealias Identity = String
+    var identity: Identity { return title }
 
     var title: String {
         switch self {
