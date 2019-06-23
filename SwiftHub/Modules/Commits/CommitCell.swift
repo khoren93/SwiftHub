ce732b035ad61f8f4aa54e2164e0a8626b5c05fb
Khoren Markosyan <khoren.markosyan@gmail.com>
2019-06-23 17:58:09 +0400

    Cells refactoring - Commits module

--- SwiftHub/Modules/Commits/CommitCell.swift
+++ SwiftHub/Modules/Commits/CommitCell.swift
@@ -7,26 +7,19 @@
 //
 
 import UIKit
+import RxSwift
 
-class CommitCell: DetailedTableViewCell {
+class CommitCell: DefaultTableViewCell {
 
     override func makeUI() {
         super.makeUI()
         leftImageView.cornerRadius = 25
     }
 
-    func bind(to viewModel: CommitCellViewModel) {
-        viewModel.title.drive(titleLabel.rx.text).disposed(by: rx.disposeBag)
-        viewModel.detail.drive(detailLabel.rx.text).disposed(by: rx.disposeBag)
-        viewModel.secondDetail.drive(secondDetailLabel.rx.text).disposed(by: rx.disposeBag)
-        viewModel.imageUrl.drive(leftImageView.rx.imageURL).disposed(by: rx.disposeBag)
-        viewModel.imageUrl.drive(onNext: { [weak self] (url) in
-            if let url = url {
-                self?.leftImageView.hero.id = url.absoluteString
-            }
-        }).disposed(by: rx.disposeBag)
-        viewModel.badge.drive(badgeImageView.rx.image).disposed(by: rx.disposeBag)
-        viewModel.badgeColor.drive(badgeImageView.rx.tintColor).disposed(by: rx.disposeBag)
+    override func bind(to viewModel: DefaultTableViewCellViewModel) {
+        super.bind(to: viewModel)
+        guard let viewModel = viewModel as? CommitCellViewModel else { return }
+        cellDisposeBag = DisposeBag()
 
         leftImageView.rx.tap().map { _ in viewModel.commit.committer }.filterNil()
             .bind(to: viewModel.userSelected).disposed(by: cellDisposeBag)
