b960cee6c301fceee5052d150a1284318c02c167
Khoren Markosyan <khoren.markosyan@gmail.com>
2020-04-16 17:55:32 +0400

    fixed badge icons filling logic

--- SwiftHub/Extensions/UIImage/UIImage+SwiftHub.swift
+++ SwiftHub/Extensions/UIImage/UIImage+SwiftHub.swift
@@ -10,5 +10,22 @@ import UIKit
 import Foundation
 
 extension UIImage {
+    func filled(withColor color: UIColor) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        color.setFill()
+        guard let context = UIGraphicsGetCurrentContext() else { return self }
 
+        context.translateBy(x: 0, y: size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setBlendMode(CGBlendMode.normal)
+
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        guard let mask = cgImage else { return self }
+        context.clip(to: rect, mask: mask)
+        context.fill(rect)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 }
